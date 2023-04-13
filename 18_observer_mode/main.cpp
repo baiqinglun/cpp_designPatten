319c4f170fee027e44a987019aa5cb34062b7491
baiqinglun <2398456523@qq.com>
2023-04-13 12:31:54 +0800

    观察者模式

---
+++ 18_observer_mode/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+//观察者  
+class Observer    
+{  
+public:  
+    Observer() {}  
+    virtual ~Observer() {}  
+    virtual void Update() {}   
+    virtual void Attach(){}
+    virtual void Remove(){}
+};
+
+//博客  
+class Blog    
+{  
+public:  
+    Blog() {}  
+    virtual ~Blog() {} 
+    //添加观察者  
+    void Attach(Observer *observer) 
+    {
+        m_observers.push_back(observer);cout << "订阅成功" <<endl;
+    }
+    //移除观察者 
+    void Remove(Observer *observer) 
+    { 
+        m_observers.remove(observer); 
+        cout << "取消订阅" <<endl;
+    }
+    //通知观察者      
+    void Notify()  
+    {  
+        list<Observer*>::iterator iter = m_observers.begin();  
+        for(; iter != m_observers.end(); iter++)  
+            (*iter)->Update();  
+    }  
+    virtual void SetStatus(string s) { m_status = s; } //设置状态  
+    virtual string GetStatus() { return m_status; }    //获得状态  
+private:  
+    list<Observer* > m_observers; //观察者链表  
+protected:  
+    string m_status; //状态  
+};
+
+//具体博客类  
+class BlogCSDN : public Blog  
+{  
+private:  
+    string m_name; //博主名称  
+public:  
+    BlogCSDN(string name): m_name(name) {}  
+    ~BlogCSDN() {}  
+    void SetStatus(string s) { m_status = "CSDN通知 : " + m_name + s; } //具体设置状态信息  
+    string GetStatus() { return m_status; }  
+};  
+//具体观察者  
+class ObserverBlog : public Observer     
+{  
+private:  
+    string m_name;  //观察者名称  
+    Blog *m_blog;   //观察的博客，当然以链表形式更好，就可以观察多个博客  
+public:   
+    ObserverBlog(string name,Blog *blog): m_name(name), m_blog(blog) {}  
+    ~ObserverBlog() {}  
+    void Update()  //获得更新状态  
+    {   
+        string status = m_blog->GetStatus();  
+        cout<<m_name<<"-------"<<status<<endl;  
+    }  
+    void Attach(){
+        m_blog->Attach(this);
+    }
+    void Remove(){
+        m_blog->Remove(this);
+    }
+};
+
+//测试案例  
+int main()  
+{  
+    Blog *blog = new BlogCSDN("博客1");  
+    Observer *observer1 = new ObserverBlog("观察者1", blog);
+    // 由粉丝调用
+    observer1->Attach();
+    // 由公众号经营者调用
+    blog->SetStatus("发表设计模式C++实现（15）——观察者模式"); 
+    // 由公众号经营者调用
+    blog->Notify();
+    // 由粉丝调用
+    observer1->Remove();
+
+    // 取消订阅后，观察者不会接受
+    blog->SetStatus("发表设计模式C++实现（16）——观察者模式"); 
+	blog->Notify();
+
+    delete blog;
+    delete observer1;  
+    return 0;  
+}
